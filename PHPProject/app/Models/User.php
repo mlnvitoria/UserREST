6729e16cb25451943b64f191120a7eeabf853491
Vitoria Mendes <mendes.tecnologia.dev@gmail.com>
2021-08-10 18:34:39 -0300

    Creating User Tag and Schema for swagger

--- PHPProject/app/Models/User.php
+++ PHPProject/app/Models/User.php
@@ -9,9 +9,50 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
+
+/**
+ * @OA\Tag(
+ *   name="user",
+ *   description="Operations about user",
+ * )
+ *
+ * @OA\Schema(@OA\Xml(name="User"))
+ * */
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
+    
+
+    /**
+     * @OA\Property(format="int64")
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $firstname;
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $lastname;
+
+    /**
+     * @var string
+     * @OA\Property()
+     */
+    public $email;
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $apiToken;
+
 
     /**
      * The attributes that are mass assignable.
