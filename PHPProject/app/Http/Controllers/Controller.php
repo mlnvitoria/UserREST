19350760fcdc64aff2e5e79a2c6ff2b20bc0564e
Vitoria Mendes <mendes.tecnologia.dev@gmail.com>
2021-08-10 18:35:13 -0300

    Add OpenAPI info for swagger

--- PHPProject/app/Http/Controllers/Controller.php
+++ PHPProject/app/Http/Controllers/Controller.php
@@ -4,6 +4,27 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *    @OA\Info(
+ *       title="UserREST PHPProject's API",
+ *       version="1.0.0",
+ *       description="This is the documentation for UserREST PHPProject's API",
+ *       termsOfService="http://swagger.io/terms/",
+ *       @OA\Contact(
+ *          email="mendes.tecnologia.dev@gmail.com"
+ *       ),
+ *       @OA\License(
+ *           name="Apache 2.0",
+ *           url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *       )
+ *     ),
+ *     @OA\Server(
+ *         description="OpenApi host",
+ *         url=API_HOST
+ *     ),
+ *   )
+ */
 class Controller extends BaseController
 {
     //
