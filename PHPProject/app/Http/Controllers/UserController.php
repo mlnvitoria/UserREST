9c7b514e5781f9c1b083ebe536fc2db997feb0df
Vitoria Mendes <mendes.tecnologia.dev@gmail.com>
2021-08-11 12:01:28 -0300

    Creating routes and controller validations, with Swagger as well

--- PHPProject/app/Http/Controllers/UserController.php
+++ PHPProject/app/Http/Controllers/UserController.php
@@ -2,17 +2,76 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    protected $repository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->repository = $userRepository;
+    }
+
+    /**
+     * Get user.
+     *
+     * @OA\Get(
+     *     path="/api/v1/user/{id}",
+     *     tags={"user"},
+     *     operationId="getUser",
+     *     summary="Get user by ID",
+     *     description="",
+     *     @OA\Parameter(
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer"
+     *          ),
+     *          description="The user ID",
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful response",
+     *         @OA\Schema(ref="#/components/schemas/User")
+     *     ),
+     *     @OA\Response(
+     *         response=401, 
+     *         description="Unauthorized",
+     *     ),
+     *     @OA\Response(
+     *         response=404, 
+     *         description="User not found",
+     *     ),
+     *     security={ {"api_key_security" : {} } }
+     * )
+     */
+    public function show(int $id) : JsonResponse
+    {
+        try {
+            $user = $this->repository->getById($id);
+            if ($user->id) {
+                return response()->json($user, 200);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => [
+                    $e->getMessage(),
+                ],
+            ], 400);
+        }
+    }
+
     /**
      * Store a new user.
      *
      * @OA\Post(
-     *     path="/user",
+     *     path="/api/v1/user",
      *     tags={"user"},
      *     operationId="addUser",
      *     summary="Add a new user to the API",
@@ -21,33 +80,58 @@ class UserController extends Controller
      *         description="User object that needs to be added to the API",
      *         required=true,
      *         @OA\MediaType(
-     *           mediaType="multipart/form-data",
+     *           mediaType="application/json",
      *           @OA\Schema(ref="#/components/schemas/User")
      *         )
      *     ), 
      *     @OA\Response(
      *         response="201",
-     *         description="successful created",
+     *         description="Successfully created",
      *         @OA\Schema(ref="#/components/schemas/ApiResponse")
      *     ),
      *     @OA\Response(
-     *         response=405,
-     *         description="Invalid input",
-     *     ),
-     *     security={{"petstore_auth":{"write:pets", "read:pets"}}}
+     *      response=400, 
+     *      description="User validation error", 
+     *      @OA\JsonContent(
+     *          @OA\Property(
+     *              property="errors",
+     *              type="array",
+     *              @OA\Items(
+     *                  type="string",
+     *                  example="The field firstname is required"
+     *              ),
+     *          ),
+     *      ),
+     *   ),
      * )
      */
-    public function store(Request $request)
+    public function store(Request $request) : JsonResponse
     {
-        $name = $request->input('name');
+        try {
+            $validator = Validator::make($request->all(), $this->repository->getValidatorRules('store'));
+            if ($validator->fails()) { 
+                return response()->json([
+                    'errors' => $validator->errors(),
+                ], 400);
+            }
 
-        //
+            $saved = $this->repository->create($request->all());
+                
+            return response()->json($saved, 201);
+            
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => [
+                    $e->getMessage(),
+                ],
+            ], 400);
+        }
     }
 
     /**
      * Update the specified user.
      *
-     * @OA\Put(path="/user/{id}",
+     * @OA\Put(path="/api/v1/user/{id}",
      *   tags={"user"},
      *   summary="Update user",
      *   description="This can only be done by the logged in user.",
@@ -58,28 +142,68 @@ class UserController extends Controller
      *     description="The user ID that need to be updated",
      *     required=true,
      *     @OA\Schema(
-     *         type="int"
+     *         type="integer"
      *     )
      *   ),
-     *   @OA\Response(response=400, description="Invalid user supplied"),
-     *   @OA\Response(response=404, description="User not found"),
      *   @OA\RequestBody(
      *       required=true,
      *       description="Updated user object",
      *       @OA\MediaType(
-     *           mediaType="multipart/form-data",
+     *           mediaType="application/json",
      *           @OA\Schema(ref="#/components/schemas/User")
      *       )
      *   ),
+     *   @OA\Response(response=204, description="User updated successfully"),
+     *   @OA\Response(
+     *      response=400, 
+     *      description="User validation error", 
+     *      @OA\JsonContent(
+     *          @OA\Property(
+     *              property="errors",
+     *              type="array",
+     *              @OA\Items(
+     *                  type="string",
+     *                  example="User not found"
+     *              ),
+     *          ),
+     *      ),
+     *   ),
+     *   @OA\Response(
+     *       response=401, 
+     *       description="Unauthorized",
+     *   ),
+     *   security={ {"api_key_security" : {} } }
      * )
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id) : JsonResponse
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), $this->repository->getValidatorRules('update'));
+            if ($validator->fails()) { 
+                return response()->json([
+                    'errors' => $validator->errors(),
+                ], 400);
+            }
+            $this->repository->update($id, $request->all());
+
+            return response()->json('', 204);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'errors' => [
+                    'User not found',
+                ],
+            ], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => [
+                    $e->getMessage(),
+                ],
+            ], 400);
+        }
     }
 
     /**
-     * @OA\Delete(path="/user/{id}",
+     * @OA\Delete(path="/api/v1/user/{id}",
      *   tags={"user"},
      *   summary="Delete user",
      *   description="This can only be done by the logged in user.",
@@ -90,14 +214,37 @@ class UserController extends Controller
      *     description="The user ID that needs to be deleted",
      *     required=true,
      *     @OA\Schema(
-     *         type="int"
+     *         type="integer"
      *     )
      *   ),
-     *   @OA\Response(response=400, description="Invalid username supplied"),
-     *   @OA\Response(response=404, description="User not found")
+     *   @OA\Response(response=202, description="The user was marked for deletion"),
+     *   @OA\Response(
+     *       response=401, 
+     *       description="Unauthorized",
+     *   ),
+     *   @OA\Response(response=404, description="User not found"),
+     *   security={ {"api_key_security" : {} } }
      * )
      */
-    public function deleteUser()
+    public function delete(int $id) : JsonResponse
     {
+        try {
+            $this->repository->deleteById($id);
+
+            return response()->json('', 202);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'errors' => [
+                    'User not found',
+                ],
+            ], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => [
+                    $e->getMessage(),
+                ],
+            ], 400);
+        }
     }
+
 }
--- PHPProject/routes/api.php
+++ PHPProject/routes/api.php
@@ -10,9 +10,16 @@
 
 
 
-$router->group(['prefix' => 'api/v1'], function() use ($router) {
-    $router->get('/', function () use ($router) {
-        return $router->app->version();
+$router->group(['prefix' => '/api/v1'], function() use ($router) {
+
+    $router->group(['prefix' => '/user'], function() use ($router) {
+        $router->post('/', 'UserController@store');
+
+        $router->group(['middleware' => 'auth'], function() use ($router) {
+            $router->get('/{id}', 'UserController@show');
+            $router->put('/{id}', 'UserController@update');
+            $router->delete('/{id}', 'UserController@delete');
+        });
     });
 });
 
