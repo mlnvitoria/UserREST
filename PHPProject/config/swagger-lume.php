55280d9de902742e816ff3fe4917b86ef420ef55
Vitoria Mendes <mendes.tecnologia.dev@gmail.com>
2021-08-11 11:59:19 -0300

    Fix Swagger information for API Token

--- PHPProject/config/swagger-lume.php
+++ PHPProject/config/swagger-lume.php
@@ -107,10 +107,10 @@ return [
         | Examples of Security definitions
         |--------------------------------------------------------------------------
         */
-        'api_key_security_example' => [ // Unique name of security
+        'api_key_security' => [ // Unique name of security
             'type' => 'apiKey', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
             'description' => 'User API Token generated at register',
-            'name' => 'api_token', // The name of the header or query parameter to be used.
+            'name' => 'X-API-TOKEN', // The name of the header or query parameter to be used.
             'in' => 'header', // The location of the API key. Valid values are "query" or "header".
         ],
         
