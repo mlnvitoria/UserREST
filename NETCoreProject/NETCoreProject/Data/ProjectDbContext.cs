79a4627204dab773bd25c62a48ef12ba2e48d649
Vitoria Mendes <mendes.tecnologia.dev@gmail.com>
2021-08-12 08:52:00 -0300

    Creating DBContext

---
+++ NETCoreProject/NETCoreProject/Data/ProjectDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETCoreProject.Models;
+
+namespace NETCoreProject.Data
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options) { }
+        public DbSet<User> User { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
